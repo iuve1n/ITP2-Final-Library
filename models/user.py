bc78f0c268293209a2ceb22ac9ade669781811d0
mtulkiba <mirasastana12345@icloud.com>
2026-05-18 00:16:09 +0500

    add user __init__ with set and history

--- models/user.py
+++ models/user.py
@@ -1,8 +1,9 @@
-"""User model (placeholder).
-
-Mostly empty per request — no implementation here.
-"""
-
 class User:
-    """Placeholder User class."""
-    pass
+    def __init__(self,user_id,name,borrowed_books_list=None):
+        self.user_id=int(user_id)
+        self.name=name
+        if borrowed_books_list==None:
+            self.acive_borrowed_books=set()
+        else:
+            self.active_borrowed_books=set(borrowed_books_list)
+        self.borrow_history=[]
