8892b3779962c7528635e3e04e4530ba5bac12e2
mtulkiba <mirasastana12345@icloud.com>
2026-05-18 00:35:38 +0500

    Add unit tests for Book

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,15 +1,18 @@
-"""Basic tests for models (placeholders).
-
-Mostly empty per request.
-"""
-
 import unittest
+from models.book import Book
+from models.user import User
+class TestBook(unittest.TestCase):
+    def test_availability_default_true(self):
+        book = Book (1,"Python","Baha",True)
+        self.assertTrue(book.get_availability())
+    def test_set_availability_false(self):
+        book = Book (1,"Python","Baha",True)
+        book.set_availability(False)
+        self.assertFalse(book.get_availability())
+    def test_private_not_accessible(self):
+        book = Book (1,"Python","Baha",True)
+        with self.assertRaises(AttributeError):
+            _= book.__is_available
 
-
-class TestModels(unittest.TestCase):
-    def test_placeholder(self):
-        self.assertTrue(True)
-
-
-if __name__ == '__main__':
+if __name__=="__main__":
     unittest.main()
